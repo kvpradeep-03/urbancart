6627097610fb24a1445a2404d22fcb05ec3539cc
kvpradeep-03 <kvpradeep60@gmail.com>
2025-08-12 18:41:28 +0530

    created navbar

---
+++ frontend/src/components/navbar/Account.jsx
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import Divider from '@mui/material/Divider';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Typography from '@mui/material/Typography';
+import { IoLogInOutline } from "react-icons/io5";
+import { IoPersonOutline } from "react-icons/io5";
+import Box from '@mui/material/Box';
+import { SvgIcon } from "@mui/material";
+import { TfiPackage } from "react-icons/tfi";
+import { GrMapLocation } from "react-icons/gr";
+ 
+
+export default function CustomizedMenus() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Box
+        id="demo-customized-button"
+        aria-controls={open ? "demo-customized-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        variant="text"
+        disableElevation
+        onClick={(e) => {
+          e.stopPropagation(); // ✅ prevents Drawer from closing
+          handleClick(e);
+        }}
+        endIcon={<KeyboardArrowDownIcon />}
+        sx={{
+          bgcolor: "#fffefe",
+          color: "#141514",
+          fontSize: { xs: 16, sm: 20 },
+          display: "flex",
+          alignItems: "center",
+          gap: 1,
+          cursor: "pointer",
+        }}
+      >
+        <IoPersonOutline />
+        <Typography sx={{textTransform: "none" }}>Profile</Typography>
+      </Box>
+      <Menu
+        disablePortal // ✅ keep inside Drawer DOM
+        elevation={1}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        id="demo-customized-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose} // important so clicking outside closes
+      >
+        <MenuItem
+          disableRipple
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start", // left align text
+            paddingY: 1,
+          }}
+        >
+          <Typography fontWeight={600}>Welcome</Typography>
+          <Typography color="initial">
+            To access account and manage orders
+          </Typography>
+        </MenuItem>
+
+        <MenuItem
+          onClick={handleClose}
+          disableRipple
+          sx={{ display: "flex", alignItems: "center", gap: 1 }}
+        >
+          <SvgIcon
+            component={IoLogInOutline}
+            inheritViewBox
+            sx={{ fontSize: { xs: 16, sm: 26 }, display: "block" }}
+          />
+          <Typography sx={{ textTransform: "none" }}>Login/Signup</Typography>
+        </MenuItem>
+
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem
+          onClick={handleClose}
+          disableRipple
+          sx={{ display: "flex", alignItems: "center", gap: 1 }}
+        >
+          <SvgIcon
+            component={TfiPackage}
+            inheritViewBox
+            sx={{ fontSize: { xs: 16, sm: 20 }, display: "block" }}
+          />
+          <Typography sx={{ textTransform: "none" }}>Orders</Typography>
+        </MenuItem>
+        <MenuItem
+          onClick={handleClose}
+          disableRipple
+          sx={{ display: "flex", alignItems: "center", gap: 1 }}
+        >
+          <SvgIcon
+            component={GrMapLocation}
+            inheritViewBox
+            sx={{ fontSize: { xs: 16, sm: 20 }, display: "block" }}
+          />
+          <Typography sx={{ textTransform: "none" }}>
+            Track My Orders
+          </Typography>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,131 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import CustomizedMenus from "./Account";
+import { GiShoppingBag } from "react-icons/gi";
+import { SvgIcon } from "@mui/material";
+import { BsShopWindow } from "react-icons/bs";
+
+const drawerWidth = 240;
+const navItems = [
+  <CustomizedMenus />,
+  <Box sx={{ display: "flex", alignItems: "center", gap: 1 ,cursor: "pointer" , mr:1}}>
+    <SvgIcon
+      component={GiShoppingBag}
+      inheritViewBox
+      sx={{ fontSize: { xs: 16, sm: 20 }, display: "block" }}
+    />
+    <Typography sx={{ textTransform: "none" }}>Bag</Typography>
+  </Box>,
+  <Box sx={{ display: "flex", alignItems: "center",cursor: "pointer" , gap: 1 }}>
+    <SvgIcon
+      component={BsShopWindow}
+      inheritViewBox
+      sx={{ fontSize: { xs: 16, sm: 20 }, display: "block" }}
+    />
+    <Typography sx={{ textTransform: "none" }}>Become a Seller</Typography>
+  </Box>,
+];
+
+const Navbar = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        UrbanCart
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar sx={{ bgcolor: "#fffefe", color: "#141514" }} component="nav">
+        <Toolbar sx={{ position: "relative" }}>
+          {/* Left Icon */}
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon sx={{ fontSize: "1.5rem" }} />
+          </IconButton>
+
+          {/* Title */}
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              // Default (desktop): normal flow
+              flexGrow: { xs: 0, sm: 1 },
+              // Mobile: absolute center
+              position: { xs: "absolute", sm: "static" },
+              left: { xs: "50%", sm: "auto" },
+              transform: { xs: "translateX(-50%)", sm: "none" },
+            }}
+          >
+            UrbanCart
+          </Typography>
+          {/* Right Navigation Buttons */}
+          <Box sx={{ display: { xs: "none", sm: "block" }, ml: "auto" }}>          
+            {navItems.map((item) => (
+              <Button key={item} sx={{ bgcolor: "#fffefe", color: "#141514" }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+};
+
+export default Navbar;
